345b5ea15afbaaea28249487f0180f629a1d93ae
William Berruyer <75248894+Gringonightz@users.noreply.github.com>
2022-01-07 17:05:57 +0100

    Delete Affiche_section_elf.h

--- Affiche_section_elf.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SECTION_ELF_WRITE_
-#define _SECTION_ELF_WRITE_
-#include <stdint.h>
-#include <stdio.h>
-#include <elf.h>
-
-void elf_print_section(const Elf32_Shdr *shdr, Elf32_Ehdr *ehdr);
-
-#endif
