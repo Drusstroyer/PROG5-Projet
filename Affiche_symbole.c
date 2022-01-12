4fbceb7f7cf74e600e15f0a86fd7c91f4d6f1962
hounyj <Julien.houny@gmail.com>
2022-01-12 15:47:21 +0100

    Affiche_symbole sers a rien

--- Affiche_symbole.c
+++
@@ -1,8 +0,0 @@
-#include "Affiche_symbole.h"
-
-void affiche_symbole(Elf32_Sym* symbole){
-    //char* tab = malloc(sizeof(char)*20);
-    printf("Name: %d\n", symbole->st_name);
-    //sprintf(tab,"%x",symbole->st_name);
-    //printf("Name: %s\n", tab);
-}
